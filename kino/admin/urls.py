39765c18ddf48e6754ecb9b8831a4fb4d5ac8f2a
Letiso <letisodianta@gmail.com>
2021-11-01 00:04:24 +0200

    A little customization for AdminLTE

--- kino/admin/urls.py
+++ kino/admin/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.admin_main, name="admin")
+    path('', views.index, name="statistics")
     ]
--- kino/admin/views.py
+++ kino/admin/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 
-def admin_main(request):
+def index(request):
     return render(request, 'admin/index.html')
