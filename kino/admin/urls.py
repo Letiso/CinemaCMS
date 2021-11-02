3650029fb986b020b4d44c3e41f65ee7cedd3cc5
Letiso <letisodianta@gmail.com>
2021-11-02 02:27:34 +0200

    Admin navbar implemented

--- kino/admin/urls.py
+++ kino/admin/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="statistics")
+    path('', views.statistics, name="statistics"),
+    path('banners', views.banners, name="banners"),
+    path('movies', views.movies, name="movies"),
+    path('cinemas', views.cinemas, name="cinemas"),
+    path('news', views.news, name="news"),
+    path('promotion', views.promotion, name="promotion"),
+    path('pages', views.pages, name="pages"),
+    path('users', views.users, name="users"),
+    path('mailing', views.mailing, name="mailing"),
     ]
--- kino/admin/views.py
+++ kino/admin/views.py
@@ -1,5 +1,37 @@
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, 'admin/index.html')
+def statistics(request):
+    return render(request, 'admin/statistics.html')
+
+
+def banners(request):
+    return render(request, 'admin/banners.html')
+
+
+def movies(request):
+    return render(request, 'admin/movies.html')
+
+
+def cinemas(request):
+    return render(request, 'admin/cinemas.html')
+
+
+def news(request):
+    return render(request, 'admin/news.html')
+
+
+def promotion(request):
+    return render(request, 'admin/promotion.html')
+
+
+def pages(request):
+    return render(request, 'admin/pages.html')
+
+
+def users(request):
+    return render(request, 'admin/users.html')
+
+
+def mailing(request):
+    return render(request, 'admin/mailing.html')
