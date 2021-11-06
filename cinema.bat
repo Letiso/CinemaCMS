2e838cb3cc351fdf4c9f67f489372171918a4b6f
Letiso <letisodianta@gmail.com>
2021-11-06 20:16:26 +0200

    From SQLite to PostgreSQL switching

---
+++ cinema.bat
@@ -0,0 +1,1 @@
+python kino\manage.py runserver
--- kino/kino/settings.py
+++ kino/kino/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'kino.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cinema',
+        'USER': 'letiso',
+        'PASSWORD': '1243',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
