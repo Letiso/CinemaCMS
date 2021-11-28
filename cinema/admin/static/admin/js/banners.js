90cbdf9475256633a4e2f56a165e713885a6eee3
Letiso <letisodianta@gmail.com>
2021-11-28 21:18:21 +0200

    Some js refactoring

--- cinema/admin/static/admin/js/banners.js
+++ cinema/admin/static/admin/js/banners.js
@@ -1,54 +1,31 @@
 
 
-const addFormBtnTopBanners = document.getElementById('top_banners-add')
-const addFormBtnNewsBanners = document.getElementById('news_banners-add')
-
-addFormBtnTopBanners.addEventListener('click', add_empty_form)
-addFormBtnNewsBanners.addEventListener('click', add_empty_form)
-
-
 function add_empty_form(event) {
     if (event) {
         event.preventDefault()
     }
-    let currentFormSet = event.target.getAttribute('name')
 
-    let totalForms = document.getElementById(`id_${currentFormSet}-TOTAL_FORMS`)
+    let currentFormSet = event.target.getAttribute('name')
+    const formset = {
+        total: `id_${currentFormSet}-TOTAL_FORMS`,
+        list: `${currentFormSet}-list`,
+        empty_form: `${currentFormSet}-empty-form`,
+        class: `card ${currentFormSet}`,
+    }
+
+    let totalForms = document.getElementById(formset.total)
     let formsCount = document.getElementsByClassName(currentFormSet).length
 
-    const formCopyTarget = document.getElementById(`${currentFormSet}-list`)
-    const copyEmptyFormEl = document.getElementById(`${currentFormSet}-empty-form`).cloneNode(true)
+    const formCopyTarget = document.getElementById(formset.list)
+    const copyEmptyFormEl = document.getElementById(formset.empty_form).cloneNode(true)
 
     const regex = new RegExp('__prefix__', 'g')
     copyEmptyFormEl.innerHTML = copyEmptyFormEl.innerHTML.replace(regex, formsCount)
 
-    copyEmptyFormEl.setAttribute('class', `card ${currentFormSet}`)
-    copyEmptyFormEl.setAttribute('id', `id_${currentFormSet}-${formsCount}-`)
+    copyEmptyFormEl.setAttribute('class', formset.class)
+    copyEmptyFormEl.setAttribute('id', `id_${currentFormSet}-${formsCount}`)
 
     totalForms.setAttribute('value', formsCount + 1)
 
     formCopyTarget.append(copyEmptyFormEl)
 }
-
-
-// const addFormBtn = document.getElementById('news-banners-add-empty-form')
-// const totalForms = document.getElementById('id_news_banners-TOTAL_FORMS')
-//
-// addFormBtn.addEventListener('click', add_empty_form)
-// function add_empty_form(event) {
-//     if (event) {
-//         event.preventDefault()
-//     }
-//     const currentForms = document.getElementsByClassName('news-banner')
-//     let currentFormCount = currentForms.length
-//     const formCopyTarget = document.getElementById('news-banners-list')
-//     const copyEmptyFormEl = document.getElementById('news-banners-empty-form').cloneNode(true)
-//     copyEmptyFormEl.setAttribute('class', 'card news-banner')
-//     copyEmptyFormEl.setAttribute('id', `id_news_banners-${currentFormCount}-`)
-//     const regex = new RegExp('__prefix__', 'g')
-//
-//     copyEmptyFormEl.innerHTML = copyEmptyFormEl.innerHTML.replace(regex, currentFormCount)
-//     totalForms.setAttribute('value', currentFormCount + 1)
-//
-//     formCopyTarget.append(copyEmptyFormEl)
-// }
