7d83fdb5778d20a8eaf0af0d1966849d03014734
Letiso <letisodianta@gmail.com>
2021-12-08 00:36:32 +0200

    front-end image input validation & dynamic thumbnail update implemented

--- cinema/admin/static/admin/js/dynamic_image_select.js
+++ cinema/admin/static/admin/js/dynamic_image_select.js
@@ -3,17 +3,36 @@ const fileInputs = document.getElementsByClassName('form-control-file')
 for (let i = 0; i < fileInputs.length; i++) {
     fileInputs[i].setAttribute('onchange', "set_thumbnail(event)")
     // fileInputs[i].addEventListener('change', set_thumbnail)
-
 }
 
 function set_thumbnail(event) {
-    const imageInput = event.currentTarget
-
+    const imageInput = event.currentTarget;
     const reader = new FileReader();
+
     reader.onload = function(){
-        const thumbnail = document.getElementById(`${imageInput.getAttribute('id')}-thumbnail`)
-        thumbnail.src = reader.result;
+        image_validation(reader.result, (is_valid) => {
+            if (is_valid) {
+                const thumbnail = document.getElementById(`${imageInput.id}-thumbnail`);
+                thumbnail.src = reader.result;
+            }
+        })
     };
 
     reader.readAsDataURL(event.target.files[0]);
   }
+
+function image_validation(src, callback) {
+    const image = new Image();
+
+    image.onload = function() {
+        console.log(image.naturalWidth, image.naturalHeight);
+
+        if (image.naturalWidth === 1000 && image.naturalHeight === 190) {
+            console.log('valid_image');
+            callback(true);
+        } else {
+            console.log('invalid_image');
+        }
+    }
+    image.src = src;
+}
