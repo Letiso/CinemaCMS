3523e2da940b640c90c8101abb5b36555f505cad
Letiso <letisodianta@gmail.com>
2022-02-01 15:47:29 +0200

    some fixes for collapses state save & restore after page reload

--- cinema/files/static/admin/js/collapses_state.js
+++ cinema/files/static/admin/js/collapses_state.js
@@ -1,33 +1,51 @@
 // init localStorage collapses record
-let collapses = localStorage.getItem('collapses')
+let COLLAPSES = localStorage.getItem('collapses')
 
-if (collapses) {
-    collapses = JSON.parse(collapses)
+if (COLLAPSES) {
+    COLLAPSES = JSON.parse(COLLAPSES)
 }
 
-if (!collapses || (collapses.name !== location.pathname)) {
-    collapses = {
+// recreate COLLAPSES object if we got an invalid data from LocalStorage
+// or just page url is changed
+if (!COLLAPSES || (COLLAPSES.name !== location.pathname)) {
+
+    COLLAPSES = {
       name: location.pathname,
       toShow: {}
     }
-    localStorage.setItem('collapses', JSON.stringify(collapses))
+
+    localStorage.setItem('collapses', JSON.stringify(COLLAPSES))
 }
 
 // restoring saved collapses state
 let key, target;
-for (key in collapses.toShow) {
-    target = document.getElementById(collapses.toShow[key]);
+for (key in COLLAPSES.toShow) {
+    target = document.getElementById(COLLAPSES.toShow[key]);
     target.setAttribute('class', `${target.getAttribute('class')} show`)
 }
 
 
 // update localStorage record about collapses
-function toggle_show(event, collapse) {
-    if (collapse in collapses.toShow) {
-      delete collapses.toShow[collapse]
+function toggle_show(event, collapse_name) {
+    if (collapse_name in COLLAPSES.toShow) {
+      delete COLLAPSES.toShow[collapse_name]
     }
     else {
-      collapses.toShow[collapse] = event.currentTarget.getAttribute('aria-controls')
+      COLLAPSES.toShow[collapse_name] = event.currentTarget.getAttribute('aria-controls')
     }
-    localStorage.setItem('collapses', JSON.stringify(collapses))
+    localStorage.setItem('collapses', JSON.stringify(COLLAPSES))
 }
+
+// init EventListener on every card collapse div
+function initCollapsesEventListener() {
+    console.log('first')
+    $("a[data-toggle*='collapse']").on('click', function (event) {
+        // toggle_show(event, $(this).prop("href"))
+        console.log(this)
+        console.log($(this).prop('href'))
+        console.log('second')
+    })
+}
+
+console.log('third')
+initCollapsesEventListener()
