dc0ed67170e345de6effad489b8e3b71c519e9b0
Letiso <letisodianta@gmail.com>
2022-02-01 18:02:34 +0200

    some refactoring of dataTable usage

---
+++ cinema/admin/static/admin/js/get_data_table.js
@@ -0,0 +1,21 @@
+
+// simply making a dataTable object and returns it; u have to give table ID
+function getDataTable (id) {
+    return $(`#${id}`).DataTable({
+        "responsive": true,
+        "lengthChange": false,
+        "autoWidth": false,
+        "language": {
+            "infoFiltered": "(Отфильтровано _MAX_ записей)",
+            "zeroRecords": "Записей не найдено",
+            "info": "Показано с _START_ по _END_ из _TOTAL_",
+            "infoEmpty": "Нет записей",
+            "search": "Поиск",
+            "paginate": {
+                "previous": "Предыдущая",
+                "last": "Последняя",
+                "next": "Следующая",
+            }
+        }
+    })
+}
