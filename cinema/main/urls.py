fb0151019e4b6b0fa7c51fe2892c8623fd77f7a9
Letiso <letisodianta@gmail.com>
2022-02-14 22:12:08 +0200

    minor fix

--- cinema/main/urls.py
+++ cinema/main/urls.py
@@ -5,16 +5,22 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.index, name="main_page"),
+
     path('poster', views.poster, name="poster"),
     path('poster/movie_card', views.movie_card, name="movie_card"),
+
     path('soon', views.soon, name="soon"),
+
     path('timetable', views.timetable, name="timetable"),
     path('timetable/ticket_booking', views.ticket_booking, name="ticket_booking"),
+
     path('cinemas', views.cinemas, name="cinemas"),
     path('cinemas/cinema_card', views.cinema_card, name="cinema_card"),
     path('cinemas/hall_card', views.hall_card, name="hall_card"),
+
     path('promotion', views.promotion, name="promotion"),
     path('promotion/promotion_card', views.promotion_card, name="promotion_card"),
+
     path('about_the_cinema', views.about_the_cinema, name="about_the_cinema"),
     path('about_the_cinema/advertising', views.advertising, name="advertising"),
     path('about_the_cinema/cafe_bar', views.cafe_bar, name="cafe_bar"),
